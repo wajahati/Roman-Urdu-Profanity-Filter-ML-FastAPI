ad1cea69fe4aeec1246dd1c11711d875cbc974e6
Wajahat Ahmad <67877858+wajahati@users.noreply.github.com>
2023-05-16 01:36:27 +0500

    Update and rename Categories.py to API.py

--- API.py
+++ API.py
@@ -6,9 +6,8 @@ Created on Sun May 14 02:12:51 2023
 """
 
 from pydantic import BaseModel
-class Category(BaseModel):
-    title: str 
-    description: str 
+class Profanity(BaseModel):
+    message: str 
     
 class Price(BaseModel):
     title: str 
